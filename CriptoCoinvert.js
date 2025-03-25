1fff5057951f4fd816a4d0acea57349ad26c83a4
Carolina De Souza Gonçalves <cahdesouzag@gmail.com>
2025-03-25 18:56:53 -0300

    first commit

--- CriptoCoinvert.js
+++
@@ -1,6 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const selectOrigem = document.getElementById("moedaOrigem");
-//     const selectDestino = document.getElementById("moedaDestino");
-
-//     selectDestino.innerHTML = selectOrigem.innerHTML;
-// });
